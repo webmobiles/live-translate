60f490651c8fe45def964a485557a1b1adb76a4a
dave i. <21120559+webmobiles@users.noreply.github.com>
2026-06-08 00:11:40 +0200

    remove tanstack div router from bottom

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,11 +1,5 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <Outlet />
-      <TanStackRouterDevtools />
-    </>
-  ),
+  component: () => <Outlet />,
 })
