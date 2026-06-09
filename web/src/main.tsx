86ac908d02cba6b891920d0bc774572df8d657cc
dave i. <21120559+webmobiles@users.noreply.github.com>
2026-06-09 18:45:01 +0200

    remove div: Tanstack query devtools

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { routeTree } from './routeTree.gen'
 import './index.css'
 
@@ -23,7 +22,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools />
     </QueryClientProvider>
   </StrictMode>,
 )
